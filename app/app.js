adfba0ac242d705f34878f0fa409ac7d41a576c5
ahang <hang.andy.t@gmail.com>
2017-06-27 19:32:30 -0500

    initial commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            <h1>My Portfolio</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+);
