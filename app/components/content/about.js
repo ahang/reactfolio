d949d1d94a9e9453e01bf4403f5457d8669799cc
ahang <hang.andy.t@gmail.com>
2017-07-14 12:23:40 -0500

    moved index to root of folder, for gh-pages

--- app/components/content/about.js
+++ app/components/content/about.js
@@ -10,7 +10,7 @@ class About extends Component {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-3">
-                        <img src="assets/img/profile.jpg" alt="me" className="profile-img mx-auto d-block img-thumbnail rounded-circle" />
+                        <img src="public/assets/img/profile.jpg" alt="me" className="profile-img mx-auto d-block img-thumbnail rounded-circle" />
                     </div>
                     <div className="col-md-9">
                         <p>Hello! I am currently a student at Northwestern Coding Bootcamp, learning how to become a Web Developer. Over the last six months, I have learned HTML, CSS, Javascript, jQuery, Node, Express, SQL and NoSQL DB, Handlebars, ReactJS. I currently work full time as a quality assurance engineer for a bankruptcy software company and will be transitioning to Web Application Developer fulltime towards the end of July. I am a very detailed-oriented person and a problem solver who is looking to switch careers and focus fully on development!</p>
